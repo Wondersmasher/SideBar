9e87bcab1cc184c269c68ac5523b66f505143c47
wondersmasher <wonderfulakinniyi@gmai.com>
2022-11-05 10:18:41 +0100

    Implemented SideBar Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from "react";
 import SideBar from "./SideBar";
-import Info from "./SideBar";
-import { FaAngleDoubleRight } from "react-icons/fa";
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = "https://course-api.com/react-tabs-project";
-// const consumerAndProvider = React.createContext()
 function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [information, setInformation] = useState([]);
@@ -26,42 +22,12 @@ function App() {
   if (isLoading) {
     return <section className="loading">Loading...</section>;
   }
-  const { id, company, duties, dates, title } = information[order];
   return (
-    <section className="section">
-      <div className="title">
-        <h2>Experience</h2>
-        <div className="underline"></div>
-      </div>
-      <div className="jobs-center">
-        <div className="btn-container">
-          {information.map((item, idx) => {
-            return (
-              <button
-                key={item.id}
-                onClick={() => handleClick(idx)}
-                className={`job-btn ${idx === order && `active-btn`}`}
-              >
-                {item.company}
-              </button>
-            );
-          })}
-        </div>
-        <article className="jobs-info">
-          <h3>{title}</h3>
-          <h4>{company}</h4>
-          <p className="job-date">{dates}</p>
-          {duties.map((duty, idx) => {
-            return (
-              <div className="job-desc" key={idx}>
-                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
-                <p>{duty}</p>
-              </div>
-            );
-          })}
-        </article>
-      </div>
-    </section>
+    <SideBar
+      handleClick={handleClick}
+      information={information}
+      order={order}
+    />
   );
 }
 
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,2 +1,41 @@
 import React from "react";
-export default function SideBar() {}
+import { FaAngleDoubleRight } from "react-icons/fa";
+export default function SideBar({ handleClick, order, information }) {
+  const { company, duties, dates, title } = information[order];
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>Experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {information.map((item, idx) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => handleClick(idx)}
+                className={`job-btn ${idx === order && `active-btn`}`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        <article className="jobs-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, idx) => {
+            return (
+              <div className="job-desc" key={idx}>
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
+}
