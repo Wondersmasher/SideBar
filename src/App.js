fcd439beb92b25ac4a6bfaf51cc65fc26c1da00e
wondersmasher <wonderfulakinniyi@gmai.com>
2022-11-07 20:35:17 +0100

    Cleaned up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from "react";
 import SideBar from "./SideBar";
-import useFetch from "./useFetch";
+// import useFetch from "./useFetch";
 const url = "https://course-api.com/react-tabs-project";
 function App() {
-  const { isLoading, information, order, setOrder } = useFetch(url);
+  const [isLoading, setIsLoading] = useState(true);
+  const [information, setInformation] = useState([]);
+  const [order, setOrder] = useState(0);
+  async function getInfo(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    setInformation(data);
+    setIsLoading(false);
+    // console.log(data);
+  }
+  useEffect(() => {
+    getInfo(url);
+  }, [url]);
+  // const { isLoading, information, order, setOrder } = useFetch(url);
   function handleClick(idx) {
     setOrder(idx);
   }
