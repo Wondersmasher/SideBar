d0da59fef6cdd6d6af937a200367bcbda878525d
wondersmasher <wonderfulakinniyi@gmai.com>
2022-11-07 20:11:48 +0100

    Integrated useFetch on this application

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React, { useState, useEffect } from "react";
 import SideBar from "./SideBar";
-
+import useFetch from "./useFetch";
 const url = "https://course-api.com/react-tabs-project";
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [information, setInformation] = useState([]);
-  const [order, setOrder] = useState(0);
-  async function getInfo() {
-    const res = await fetch(url);
-    const data = await res.json();
-    setInformation(data);
-    setIsLoading(false);
-    console.log(data);
-  }
-  useEffect(() => {
-    getInfo();
-  }, []);
+  const { isLoading, information, order, setOrder } = useFetch(url);
   function handleClick(idx) {
     setOrder(idx);
   }
